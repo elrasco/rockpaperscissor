22de8b9edd5d196b3f66409ada415813823f5726
luca.rasconi <luca.rasconi@gmail.com>
2014-12-07 19:03:54 +0100

    update MovesQueue

--- rps/src/main/java/it/lucarasconi/game/MovesQueue.java
+++ rps/src/main/java/it/lucarasconi/game/MovesQueue.java
@@ -8,10 +8,16 @@ public class MovesQueue {
 
 	private static Queue<Move> moves = new ConcurrentLinkedQueue<Move>();
 	
+	/**
+	 * return the first move, in a FIFO queue, not played by the player. Random play otherwise.
+	 * @param p
+	 * @return
+	 */
 	public static Move getAValidMoveFor(Player p) {
 		for (Move move : moves) {
 			if (!move.getPlayer().isEqual(p)) {
-				return moves.poll();
+				moves.remove(move);
+				return move;
 			}
 		}
 		return new Move(new Player("randomplayer_" + new Date().getTime()), Sign.randomSign());
